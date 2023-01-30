860efcdd14f25e285f8c32b7f283b4dbfc8124ba
Ataur-rahman-01 <iamataur0911@gmail.com>
2023-01-30 13:29:08 +0600

    full snake game using python

---
+++ scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"Score: {self.score}", align='center', font=('Arial', 20, 'normal'))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align='center', font=('Arial', 25, 'normal'))
+
+    def increase(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
+
